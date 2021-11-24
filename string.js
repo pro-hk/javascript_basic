9561daf335395d331e3a85a588f12a39f915a150
janghyeok <pro_hk@naver.com>
2021-11-24 16:05:12 +0900

    로또

---
+++ string.js
@@ -0,0 +1,9 @@
+const msg = "Hello JavaScript";
+const splitMsg = msg.split("");
+console.log(splitMsg);
+console.log(splitMsg[0]);
+const tel = "010-2582-2242";
+const splitTel = tel.split("-").join("");
+console.log(splitTel);
+
+// 구분자로 나누어짐
